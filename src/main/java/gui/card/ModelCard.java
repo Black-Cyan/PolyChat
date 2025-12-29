1b6e54f2194f47accbc184a247d47620e5bec8a5
墨青 <117635344+Black-Cyan@users.noreply.github.com>
2025-12-29 15:51:08 +0800

    Revert "Enhance dialog focus handling and mouse click logic"

--- src/main/java/gui/card/ModelCard.java
+++ src/main/java/gui/card/ModelCard.java
@@ -134,8 +134,7 @@ public class ModelCard extends JPanel {
 
             @Override
             public void mouseClicked(MouseEvent e) {
-                // 只有左键点击才打开聊天窗口
-                if (chatDAO != null && e.getButton() == MouseEvent.BUTTON1) {
+                if (chatDAO != null) {
                     new ChatWindow(model, chatDAO).setVisible(true);
                 }
             }
